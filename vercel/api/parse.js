0e3c11d80083b8b97b5614378d115bc015a47608
tagproDAD <207278099+tagproDAD@users.noreply.github.com>
2025-12-03 13:07:55 -0500

    directorys

--- vercel/api/parse.js
+++ vercel/api/parse.js
@@ -17,6 +17,7 @@ export default async function handler(req, res) {
     }
 
     if (!record) {
+      console.log("no cap found");
       return res.status(400).json({ error: "No valid cap found" });
     }
 
